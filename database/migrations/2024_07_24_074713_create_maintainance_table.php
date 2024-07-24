857344a9dd0320906951fb40736a7598cce3d6cc
mikaelendale <149874638+mikaelendale@users.noreply.github.com>
2024-07-24 13:48:53 +0300

    Create 2024_07_24_074713_create_maintainance_table.php

---
+++ database/migrations/2024_07_24_074713_create_maintainance_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('maintainance', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->string('time_line_status')->nullable();
+            $table->string('time_line_1')->nullable();
+            $table->string('time_line_1_description')->nullable();
+            $table->string('time_line_1_link')->nullable();
+            $table->string('time_line_2')->nullable();
+            $table->string('time_line_2_description')->nullable();
+            $table->string('time_line_2_link')->nullable();
+            $table->string('time_line_3')->nullable();
+            $table->string('time_line_3_description')->nullable();
+            $table->string('time_line_3_link')->nullable();
+            $table->string('time_line_4')->nullable();
+            $table->string('time_line_4_description')->nullable();
+            $table->string('time_line_4_link')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('maintainance');
+    }
+};
