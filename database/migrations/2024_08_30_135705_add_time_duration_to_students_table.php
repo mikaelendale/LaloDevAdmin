dd7c528b1cb85d021117037b70a6b0bf1af0f23e
mikaelendale <149874638+mikaelendale@users.noreply.github.com>
2024-09-07 07:19:28 +0300

    Create 2024_08_30_135705_add_time_duration_to_students_table.php

---
+++ database/migrations/2024_08_30_135705_add_time_duration_to_students_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->time('time_duration')->nullable()->after('dob');
+            // 'time' data type to store the duration in HH:MM:SS format
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('time_duration');
+        });
+    }
+};
